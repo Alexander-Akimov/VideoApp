61244cfd4496bfa8c9aa22ae23eecacbf5eebf91
Alexander <a.akimov@dostek.kg>
2019-08-09 17:19:28 +0600

    Using Glade with Dagger

--- app/src/main/java/com/use/dagger/AuthActivity.kt
+++ app/src/main/java/com/use/dagger/AuthActivity.kt
@@ -1,9 +1,13 @@
 package com.use.dagger
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_auth.*
 import javax.inject.Inject
 
 class AuthActivity : DaggerAppCompatActivity() {
@@ -12,17 +16,22 @@ class AuthActivity : DaggerAppCompatActivity() {
         private val TAG = AuthActivity::class.simpleName
     }
 
-    @Inject
-    lateinit var someString: String
+    var logo: Drawable? = null
+        @Inject set
 
-    var isAppNull: Boolean? = null
-        @Inject set/**/
+    lateinit var requestManager: RequestManager
+        @Inject set
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        Log.d(TAG, "onCreate: $someString")
-        Log.d(TAG, "onCreate: is app null? $isAppNull")
+        setLogo()
+    }
+
+    private fun setLogo() {
+        requestManager
+            .load(logo)
+            .into(login_logo)
     }
 }
--- app/src/main/java/com/use/dagger/di/AppModule.kt
+++ app/src/main/java/com/use/dagger/di/AppModule.kt
@@ -1,6 +1,12 @@
 package com.use.dagger.di
 
 import android.app.Application
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
+import com.use.dagger.R
 import dagger.Module
 import dagger.Provides
 
@@ -11,11 +17,20 @@ public class AppModule {
     companion object {
         @JvmStatic
         @Provides
-        fun someString() = "this is a test string"
-    }
+        fun provideRequestOptions(): RequestOptions =
+            RequestOptions
+                .placeholderOf(R.drawable.white_background)
+                .error(R.drawable.white_background)
 
-    @Provides
-    fun getApp(application: Application?): Boolean {
-        return application == null
+        @JvmStatic
+        @Provides
+        fun provideGlideInstance(application: Application, requestOptions: RequestOptions): RequestManager =
+            Glide.with(application)
+                .setDefaultRequestOptions(requestOptions)
+
+        @Provides
+        @JvmStatic
+        fun provideAppDrawable(application: Application): Drawable? =
+            ContextCompat.getDrawable(application, R.drawable.logo)
     }
 }
